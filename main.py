831a7bafa93a3ca068b1932f1ed22ff3b42f3279
Christina Zhang <christinaytzhangroxs@gmail.com>
2021-04-14 20:14:05 -0400

    Working sprite drag and drop
    Got drag and drop to work. Renamed variables, classes and functions.

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import random
 import pygame
 
 ### Setting up dimensions
-winSize = 400
-rectSize = 50 #must be a factor of winSize
-numRect = (int)(winSize/rectSize)
+win_size = 400
+rect_size = 50 #must be a factor of win_size
+num_rect = (int)(win_size/rect_size)
+done = False
+sprite_list = pygame.sprite.Group()
+sprite_list2 = pygame.sprite.GroupSingle(sprite=None)
 
 ### initialisation
 pygame.init()
-window = pygame.display.set_mode((winSize, winSize))
+window = pygame.display.set_mode((win_size, win_size))
 pygame.display.set_caption("Gradient Rect")
 
 ### functions
@@ -24,55 +27,93 @@ def gradientRect(window, topleft_colour, topright_colour, bottomright_colour, bo
     colour_rect = pygame.transform.smoothscale(colour_rect, (target_rect.width, target_rect.height ))  # stretch!
     window.blit(colour_rect, target_rect)                                    # paint it
 
-def getColours(winDimensions, rectDimensions):
-    x = 0
+def getColours(win_dimensions, rect_dimensions):
+    count = 0
     list = []
-    for j in range(0, winDimensions-1, rectDimensions):
-        for i in range(0, winDimensions-1, rectDimensions):
-            list.insert(x, pygame.Surface.get_at(window, (j, i)))
-            x = x + 1
+    for i in range(0, win_dimensions-1, rect_dimensions):
+        for j in range(0, win_dimensions-1, rect_dimensions):
+            list.insert(count, pygame.Surface.get_at(window, (i, j)))
+            count += 1
     return list
 
-def shuffle(list, numRect):
-    shuffledList = list[1:numRect-1] + list [numRect:-numRect] + list[-(numRect-1):-1]
-    random.shuffle(shuffledList)
-    return shuffledList
+def shuffle(list, num_rect):
+    shuffled_list = list[1:num_rect-1] + list[num_rect:-num_rect] + list[-(num_rect-1):-1]
+    random.shuffle(shuffled_list)
+    return shuffled_list
 
-def replace(shuffledList, list, numRect) :
-    list[1:numRect-1] = shuffledList[0:numRect-2]
-    list[numRect:-numRect] = shuffledList[numRect-2:-(numRect-2)]
-    list[-(numRect-1):-1] = shuffledList[-(numRect-2):]
+def replace(shuffled_list, list, num_rect) :
+    list[1:num_rect-1] = shuffled_list[0:num_rect-2]
+    list[num_rect:-num_rect] = shuffled_list[num_rect-2:-(num_rect-2)]
+    list[-(num_rect-1):-1] = shuffled_list[-(num_rect-2):]
     return list
 
-def draw(list, winDimensions, rectDimensions):
-    x = 0
-    for j in range (0, winDimensions-1, rectDimensions):
-        for i in range (0, winDimensions-1, rectDimensions):
-            pygame.draw.rect(window, list[x], (j, i, rectDimensions, rectDimensions))
-            x=x+1
+def draw(list, win_dimensions, rect_dimensions):
+    count = 0
+    for i in range (0, win_dimensions-1, rect_dimensions):
+        for j in range (0, win_dimensions-1, rect_dimensions):
+            pygame.draw.rect(window, list[count], (i, j, rect_dimensions, rect_dimensions))
+            count += 1
+
+def spriteGroup(shuffled_colour_list, win_size, rect_size, sprite_list):
+    count = 0
+    for i in range(0, win_size - 1, rect_size):
+        for j in range(0, win_size - 1, rect_size):
+            sprite_list.add(RectSprite(i, j, shuffled_colour_list[count], len(sprite_list) + 1))
+            count += 1
+    return sprite_list
 
 ### creating gradiented rectangles
-gradientRect(window, (255, 0, 0), (255, 255, 255), (0, 0, 0), (0, 72, 255), pygame.Rect(0, 0, winSize, winSize))
-
-colourList = getColours(winSize, rectSize)
-draw(colourList, winSize, rectSize)
+gradientRect(window, (255, 0, 0), (255, 255, 255), (0, 0, 0), (0, 72, 255), pygame.Rect(0, 0, win_size, win_size))
+colour_list = getColours(win_size, rect_size)
+draw(colour_list, win_size, rect_size)
+shuffled_colour_list = replace(shuffle(colour_list, num_rect), colour_list, num_rect)
 pygame.display.update()
 pygame.time.delay(1000)
 
-#print(colourList)
-#print(shuffle(colourList, numRect))
-#print(replace(shuffle(colourList, numRect), colourList, numRect))
 
-shuffledColourList = replace(shuffle(colourList, numRect), colourList, numRect)
-draw(shuffledColourList, winSize, rectSize)
+class RectSprite(pygame.sprite.Sprite):
 
-pygame.display.update()
+    def __init__(self, x_pos, y_pos, colour, id):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([50, 50])
+        self.image.fill(colour)
+        self.clicked = False
+        self.rect = self.image.get_rect()
+        self.rect.y = y_pos
+        self.rect.x = x_pos
 
-### Main Loop
-game_over = False
-while not game_over:
+# drawing the thing
+spriteGroup(shuffled_colour_list, win_size, rect_size, sprite_list)
 
-    # Handle user-input
+while not done:
+    sprite_list.draw(window)
+
+    # pygame.display.flip()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sys.exit()
+            done = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            x = pos[0]
+            y = pos[1]
+            if event.button == 1:
+                for sprite in sprite_list:
+                    if sprite.rect.collidepoint(pos):
+                        sprite.clicked = True
+                        sprite_list2.add(sprite)
+                        sprite.clicked = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            for sprite in sprite_list:
+                sprite.clicked = False
+
+    for sprite in sprite_list2:
+        if sprite.clicked == True:
+            pos = pygame.mouse.get_pos()
+            sprite.rect.x = pos[0] - sprite.rect.width / 2
+            sprite.rect.y = pos[1] - sprite.rect.height / 2
+    sprite_list2.draw(window)
+
+    pygame.display.flip()
+
+pygame.quit()
