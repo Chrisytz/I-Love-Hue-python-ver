9787aba01d897de5849e6bd87985ba3850d91e89
Christina Zhang <christinaytzhangroxs@gmail.com>
2021-04-24 14:19:40 -0400

    Reformatting T^T

--- main.py
+++ main.py
@@ -2,27 +2,11 @@
 # ♪(ϞϞ(๑⚈ ․̫ ⚈๑)∩
 # April 2021
 
-
 import sys
 import random
 import pygame
 from pygame.locals import *
 
-win_width = 600
-win_height = 400
-done = False
-pygame.init()
-window = pygame.display.set_mode((win_width, win_height))
-pygame.display.set_caption("Gradient Rect")
-
-colour_list = [[(0,0,255), (0,255,0), (255,0,255), (255,255,0)],[(255,0,255), (0,255,0), (0,0,255), (255,255,0)],[(0,255,255), (0,255,0), (255,0,255), (255,255,0)]]
-sprite_list = pygame.sprite.Group()
-
-def update_sprites(sprite_list):
-    pygame.draw.rect(window, (0, 0, 0), (0, 0, 200, 400))
-    sprite_list.draw(window)
-    pygame.draw.rect(window, (0, 0, 0), (0, 0, 200, 20))
-    pygame.draw.rect(window, (0, 0, 0), (0, 380, 200, 20))
 
 class Rect(pygame.sprite.Sprite):
     def __init__(self, x_pos, y_pos, colour):
@@ -35,40 +19,70 @@ class Rect(pygame.sprite.Sprite):
         self.rect.y = y_pos
         self.colour = colour
 
-def sprites (colour_list, sprite_list):
+
+def updateSprites(sprite_list, window, win_height, width_sidebar, bar_thickness, bottom_bar_loc):
+    pygame.draw.rect(window, (0, 0, 0), (0, 0, width_sidebar, win_height))
+    sprite_list.draw(window)
+    pygame.draw.rect(window, (0, 0, 0), (0, 0, width_sidebar, bar_thickness))
+    pygame.draw.rect(window, (0, 0, 0), (0, bottom_bar_loc, width_sidebar, bar_thickness))
+
+
+def addSidebarSprites(sprite_list, colour_list, bar_thickness, rect_size):
     for i in range(0, 3):
-        for j in range (0, 4):
-            sprite_list.add(Rect(20+(40*j), (20*(i+1)) + (40*i), colour_list[i][j]))
+        for j in range(0, 4):
+            sprite_list.add(
+                Rect(bar_thickness + (rect_size * j), (bar_thickness * (i + 1)) + (rect_size * i), colour_list[i][j]))
     return sprite_list
 
-sprites(colour_list, sprite_list)
-
-sprite_list.draw(window)
-
-pygame.display.update()
 
 # This is the main entry point to the game.
-while not done:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        elif event.type == MOUSEWHEEL:
-            if pygame.mouse.get_pos()[0] < 200:
-                if event.y == -1:
-                    for sprite in sprite_list:
-                        sprite.rect.y -= 20
-                        print (sprite.rect.y)
-                    update_sprites(sprite_list)
-                    pygame.display.flip()
-                else:
-                    for sprite in sprite_list:
-                        sprite.rect.y += 20
-                        print (sprite.rect.y)
-                    update_sprites(sprite_list)
-                    pygame.display.flip()
+def sidebar():
+    # init
+    win_width = 600
+    win_height = 400
+    done = False
+    pygame.init()
+    window = pygame.display.set_mode((win_width, win_height))
+    pygame.display.set_caption("Gradient Rect")
 
-pygame.quit()
+    colour_list = [[(0, 0, 255), (0, 255, 0), (255, 0, 255), (255, 255, 0)],
+                   [(255, 0, 255), (0, 255, 0), (0, 0, 255), (255, 255, 0)],
+                   [(0, 255, 255), (0, 255, 0), (255, 0, 255), (255, 255, 0)]]
+    sprite_list = pygame.sprite.Group()
+
+    # calculating variables
+    bar_thickness = win_height * 0.05
+    rect_size = ((win_width / 3) - (win_height * 0.1)) / 4
+    width_sidebar = win_width / 3
+    bottom_bar_loc = win_height * 0.95
+
+    # displaying sprites
+    sprite_list = addSidebarSprites(sprite_list, colour_list, bar_thickness, rect_size)
+    sprite_list.draw(window)
+    pygame.display.update()
+
+    # running the game
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            elif event.type == MOUSEWHEEL:
+                if pygame.mouse.get_pos()[0] < win_width / 3:
+                    if event.y == -1:
+                        for sprite in sprite_list:
+                            sprite.rect.y -= 20
+                            print(sprite.rect.y)
+                        updateSprites(sprite_list, window, win_height, width_sidebar, bar_thickness, bottom_bar_loc)
+                        pygame.display.flip()
+                    else:
+                        for sprite in sprite_list:
+                            sprite.rect.y += 20
+                            print(sprite.rect.y)
+                        updateSprites(sprite_list, window, win_height, width_sidebar, bar_thickness, bottom_bar_loc)
+                        pygame.display.flip()
+    pygame.quit()
+
 
 # create loading screen!
 
@@ -79,3 +93,4 @@ pygame.quit()
 
 if __name__ == "__main__":
     print("This is the main file!")
+    sidebar()
