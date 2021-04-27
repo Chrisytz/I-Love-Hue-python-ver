db422c236632f646e3fdc41534fec73ba275e287
Unknown <czhan7@ocdsb.ca>
2021-04-26 20:00:16 -0400

    it doesnt work :)

--- test.py
+++ test.py
@@ -1,48 +1,104 @@
+# The original colourgame coded by Christina Zhang, April Break 2021
+
+import sys
+import random
 import pygame
+
+### initiating variables
+size = 600, 400
+win_size = 800
+rect_size = 200  # must be a factor of win_size
+num_rect = (int)(win_size / rect_size)
+done = False
+sprite_list = pygame.sprite.Group()
+sprite_list2 = pygame.sprite.GroupSingle(sprite=None)
+
+
+### initialisation
 pygame.init()
+window = pygame.display.set_mode((win_size, win_size))
+pygame.display.set_caption("Gradient Rect")
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255, 50)  # This color contains an extra integer. It's the alpha value.
-PURPLE = (255, 0, 255)
+class Circle(pygame.sprite.Sprite):
+    def __init__(self, colour, x_pos, y_pos, win_vars, id):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((win_vars["circle_size"], win_vars["circle_size"]))
+        self.image.fill((0, 0, 0))
+        self.colour = colour
+        self._original_colour = colour
+        self.rect = self.image.get_rect()
+        self.rect.x = x_pos
+        self.rect.y = y_pos
+        self.clicked = False
+        self.id = id
 
-screen = pygame.display.set_mode((200, 325))
-screen.fill(WHITE)  # Make the background white. Remember that the screen is a Surface!
-clock = pygame.time.Clock()
+    def getOriginalColour(self):
+        return self._original_colour
 
-size = (50, 50)
-red_image = pygame.Surface(size)
-green_image = pygame.Surface(size)
-blue_image = pygame.Surface(size, pygame.SRCALPHA)  # Contains a flag telling pygame that the Surface is per-pixel alpha
-purple_image = pygame.Surface(size)
+    def drawCircle(self, screen):
+        pygame.draw.ellipes(screen, self.colour, self.rect)
 
-red_image.set_colorkey(BLACK)
-green_image.set_alpha(50)
-# For the 'blue_image' it's the alpha value of the color that's been drawn to each pixel that determines transparency.
-purple_image.set_colorkey(BLACK)
-purple_image.set_alpha(50)
+colour_list_circle = [(255, 0, 0), (0, 0, 255), (255, 255, 0)]
+circle_sprite_list = pygame.sprite.Group()
 
-pygame.draw.rect(red_image, RED, red_image.get_rect(), 10)
-pygame.draw.rect(green_image, GREEN, green_image.get_rect(), 10)
-pygame.draw.rect(blue_image, BLUE, blue_image.get_rect(), 10)
-pygame.draw.rect(purple_image, PURPLE, purple_image.get_rect(), 10)
+win_vars = {
+    "bar_thickness": size[1] * 0.05,
+    "sprite_size": ((size[0] / 3) - (size[1] * 0.1)) / 4,
+    "width_sidebar": size[0] / 3,
+    "bottom_bar_loc": size[1] * 0.95,
+    "black": (0, 0, 0),
+    "num_of_rectangles": 3,
+    "circle_size": ((size[0] * (2 / 3)) - (4 * size[1] * 0.05)) / 3,
+    "space_between_circles": size[1] * 0.05
+}
 
-while True:
-    clock.tick(60)
+
+### functions
+def gradientRect(window, topleft_colour, topright_colour, bottomright_colour, bottomleft_colour, target_rect):
+    """ Draw a horizontal-gradient filled rectangle covering <target_rect> """
+    colour_rect = pygame.Surface((2, 2))  # tiny! 2x2 bitmap
+    pygame.draw.line(colour_rect, topleft_colour, (0, 0), (0, 0))
+    pygame.draw.line(colour_rect, bottomright_colour, (1, 1), (1, 1))
+    pygame.draw.line(colour_rect, topright_colour, (1, 0), (1, 0))
+    pygame.draw.line(colour_rect, bottomleft_colour, (0, 1), (0, 1))
+    colour_rect = pygame.transform.smoothscale(colour_rect, (300, 300))  # stretch!
+    window.blit(colour_rect, target_rect)# paint it
+
+def addCircleSprites(colour_list_circle, circle_sprites, win_vars):
+    for i in range(0, 3):
+        for j in range(0, 3):
+            circle_sprites.add(Circle(colour_list_circle[i],
+                                      (win_vars["width_sidebar"] + win_vars["bar_thickness"]) + i * (
+                                              win_vars["circle_size"] + win_vars["space_between_circles"]),
+                                      win_vars["bar_thickness"] + j * (
+                                              win_vars["circle_size"] + win_vars["space_between_circles"]),
+                                      win_vars, i))
+    return circle_sprites
+
+def drawCircles(window, colour_list_circle, circle_sprites):
+    for sprite in circle_sprites:
+        pygame.draw.ellipse(window, colour_list_circle[0], sprite.rect)
+### creating gradiented rectangles
+
+gradientRect(window, (245, 255, 250), (255, 247, 235), (181, 27, 58), (17, 51, 173),
+             pygame.Rect(0, 0, win_size, win_size))
+
+circle_sprite_list = addCircleSprites(colour_list_circle, circle_sprite_list, win_vars)
+
+circle_sprite_list.draw(window)
+
+surface = drawCircles(window, colour_list_circle, circle_sprite_list)
+
+
+gradientRect(window, (245, 255, 250), (255, 247, 235), (181, 27, 58), (17, 51, 173), surface)
+
+
+pygame.display.update()
+
+while not done:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                screen.blit(red_image, (75, 25))
-            elif event.key == pygame.K_2:
-                screen.blit(green_image, (75, 100))
-            elif event.key == pygame.K_3:
-                screen.blit(blue_image, (75, 175))
-            elif event.key == pygame.K_4:
-                screen.blit(purple_image, (75, 250))
+            done = True
 
-    pygame.display.update()
+pygame.quit()
